1ee1496439f4dcd654df0a1f119b05a8288e3dd2
Teemu Pääkkönen <teemu@flockler.com>
2015-05-04 00:43:26 +0300

    Remove result list handling from Query constructor

--- query.py
+++ query.py
@@ -8,18 +8,19 @@ from has_actions import HasActions
 
 
 class Query(DataRecord, HasActions):
-  def __init__(self, query_id, topic = None, result_list = None, user = None, condition = None, autocomplete = None, query_text = None):
+  def __init__(self, query_id, topic = None, user = None, condition = None, autocomplete = None, query_text = None):
     DataRecord.__init__( self, uint16(query_id) )
     self.topic = topic
-    self.result_list = result_list
     self.user = user
     self.condition = condition
     self.autocomplete = bool_(autocomplete)
     self.query_text = query_text
-    if result_list is None: self.result_list = QueryResultList(self)
+    self.result_list = QueryResultList(self)
 
   def add_to_result_list( self, rank, document ):
     self.result_list.add( rank, document )
 
   def results_up_to_rank( self, rank ):
+    if int(rank) < 1 or int(rank) > self.result_list.length():
+        raise RuntimeError("Attempted to fetch results up to rank %s for query %s, which is impossible." % (rank, self.record_id))
     return self.result_list.results_up_to_rank( rank )
