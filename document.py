fa21f4081bef036dc2eb163becd26d7dbd4ef029
Teemu Pääkkönen <teemu@flockler.com>
2015-05-04 00:42:17 +0300

    Use string keys for document's relevance dict

--- document.py
+++ document.py
@@ -1,3 +1,6 @@
+import sys
+
+
 from numpy import uint16
 
 
@@ -10,13 +13,13 @@ class Document(DataRecord):
     self.relevances = {}
 
   def add_relevance(self, relevance):
-    self.relevances[ relevance.topic.record_id ] = relevance
+    self.relevances[ str(relevance.topic.record_id) ] = relevance
 
   def get_relevance_for_topic(self, topic):
     return self.get_relevance_for_topic_id( topic.record_id )
 
   def get_relevance_for_topic_id(self, topic_id):
-    return self.relevances.get( uint16(topic_id) )
+    return self.relevances.get( str(topic_id) )
 
   def is_relevant_for_topic(self, topic):
     return self.is_relevant_for_topic_id( topic.record_id )
@@ -28,7 +31,10 @@ class Document(DataRecord):
     return self.is_highly_relevant_for_topic_id( topic.record_id )
 
   def is_highly_relevant_for_topic_id(self, topic_id):
-    return self.get_relevance_for_topic_id( topic_id ).is_highly_relevant()
+    relevance = self.get_relevance_for_topic_id( topic_id )
+    if relevance is None:
+      raise RuntimeError( "ERROR: No relevance data found for document %s, topic %s. Available for topics: %s\n" % (self.record_id, topic_id, self.relevances.keys()) )
+    return relevance.is_highly_relevant()
 
   def is_moderately_relevant_for_topic(self, topic):
     return self.is_moderately_relevant_for_topic_id( topic.record_id )
