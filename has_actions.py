25593b4a178c945b9f74509d33413528788215be
Teemu Pääkkönen <teemu@flockler.com>
2015-05-12 01:02:44 +0300

    Move action inspecting methods to HasAction mixin

--- has_actions.py
+++ has_actions.py
@@ -4,3 +4,20 @@ class HasActions:
       self.actions = []
     self.actions.extend( actions )
     self.actions = sorted( self.actions, key = lambda action: action.timestamp )
+
+  def actions_by_type( self, action_type ):
+    return [(idx,action) for idx, action in enumerate(self.actions) if action.action_type == action_type]
+
+  def action_duration_in_seconds_for( self, idx, action, end_action_type = None ):
+    next_timestamp = None
+    if end_action_type is None:
+      next_timestamp = self.actions[ idx + 1 ].timestamp
+    else:
+      for ii in range( idx + 1, len(self.actions) - 1, 1 ):
+        next_action = self.actions[ii]
+        if next_action.action_type == end_action_type:
+          next_timestamp = next_action.timestamp
+          break
+    current_timestamp = action.timestamp
+    delta = next_timestamp - current_timestamp
+    return delta.total_seconds()
--- session.py
+++ session.py
@@ -61,23 +61,6 @@ class Session(DataRecord, HasActions):
     delta = last_timestamp - first_timestamp
     return delta.total_seconds()
 
-  def actions_by_type( self, action_type ):
-    return [(idx,action) for idx, action in enumerate(self.actions) if action.action_type == action_type]
-
-  def action_duration_in_seconds_for( self, idx, action, end_action_type = None ):
-    next_timestamp = None
-    if end_action_type is None:
-      next_timestamp = self.actions[ idx + 1 ].timestamp
-    else:
-      for ii in range( idx + 1, len(self.actions) - 1, 1 ):
-        next_action = self.actions[ii]
-        if next_action.action_type == end_action_type:
-          next_timestamp = next_action.timestamp
-          break
-    current_timestamp = action.timestamp
-    delta = next_timestamp - current_timestamp
-    return delta.total_seconds()
-
   def average_document_reading_time_in_seconds(self):
     read_actions = self.actions_by_type( 'DOC_MARKED_VIEWED' )
     read_times = {}
