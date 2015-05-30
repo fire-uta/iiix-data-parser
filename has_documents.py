e9ca59ed7003c0e5931434e311ddec930e97a42e
Teemu Pääkkönen <teemu@flockler.com>
2015-05-30 19:58:26 +0300

    Move document-handling methods to a has-documents mixin class

---
+++ has_documents.py
@@ -0,0 +1,94 @@
+from filterable import Filterable
+
+
+class HasDocuments:
+
+  def add_seen_documents(self, *documents):
+    if not hasattr( self, 'seen_documents' ):
+      self.seen_documents = {}
+    for document in documents:
+      self.seen_documents[ document.record_id ] = document
+
+  def add_viewed_documents( self, *documents ):
+    if not hasattr( self, 'viewed_documents' ):
+      self.viewed_documents = {}
+    for document in documents:
+      self.viewed_documents[ document.record_id ] = document
+
+  def add_marked_relevant_documents( self, *documents ):
+    if not hasattr( self, 'marked_relevant_documents' ):
+      self.marked_relevant_documents = {}
+    for document in documents:
+      self.marked_relevant_documents[ document.record_id ] = document
+
+  def seen_highly_relevant_documents(self):
+    return [document for document in self.seen_documents.values() if document.is_highly_relevant_for_topic( self.topic )]
+
+  def seen_moderately_relevant_documents(self):
+    return [document for document in self.seen_documents.values() if document.is_moderately_relevant_for_topic( self.topic )]
+
+  def seen_non_relevant_documents(self):
+    return [document for document in self.seen_documents.values() if not document.is_relevant_for_topic( self.topic )]
+
+  def viewed_highly_relevant_documents(self):
+    return [document for document in self.viewed_documents.values() if document.is_highly_relevant_for_topic( self.topic )]
+
+  def viewed_moderately_relevant_documents(self):
+    return [document for document in self.viewed_documents.values() if document.is_moderately_relevant_for_topic( self.topic )]
+
+  def viewed_non_relevant_documents(self):
+    return [document for document in self.viewed_documents.values() if not document.is_relevant_for_topic( self.topic )]
+
+  def marked_highly_relevant_documents(self):
+    return [document for document in self.marked_relevant_documents.values() if document.is_highly_relevant_for_topic( self.topic )]
+
+  def marked_moderately_relevant_documents(self):
+    return [document for document in self.marked_relevant_documents.values() if document.is_moderately_relevant_for_topic( self.topic )]
+
+  def marked_non_relevant_documents(self):
+    return [document for document in self.marked_relevant_documents.values() if not document.is_relevant_for_topic( self.topic )]
+
+  @classmethod
+  def amount_of_seen_highly_relevant_documents(cls, filter_func = Filterable.identity_filter):
+    sessions = cls.get_store().values()
+    return reduce( lambda acc, session: acc + len(session.seen_highly_relevant_documents()), filter(filter_func, sessions), 0 )
+
+  @classmethod
+  def amount_of_viewed_highly_relevant_documents(cls, filter_func = Filterable.identity_filter):
+    sessions = cls.get_store().values()
+    return reduce( lambda acc, session: acc + len(session.viewed_highly_relevant_documents()), filter(filter_func, sessions), 0 )
+
+  @classmethod
+  def amount_of_marked_highly_relevant_documents(cls, filter_func = Filterable.identity_filter):
+    sessions = cls.get_store().values()
+    return reduce( lambda acc, session: acc + len(session.marked_highly_relevant_documents()), filter(filter_func, sessions), 0 )
+
+  @classmethod
+  def amount_of_marked_moderately_relevant_documents(cls, filter_func = Filterable.identity_filter):
+    sessions = cls.get_store().values()
+    return reduce( lambda acc, session: acc + len(session.marked_moderately_relevant_documents()), filter(filter_func, sessions), 0 )
+
+  @classmethod
+  def amount_of_marked_non_relevant_documents(cls, filter_func = Filterable.identity_filter):
+    sessions = cls.get_store().values()
+    return reduce( lambda acc, session: acc + len(session.marked_non_relevant_documents()), filter(filter_func, sessions), 0 )
+
+  @classmethod
+  def amount_of_seen_moderately_relevant_documents(cls, filter_func = Filterable.identity_filter):
+    sessions = cls.get_store().values()
+    return reduce( lambda acc, session: acc + len(session.seen_moderately_relevant_documents()), filter(filter_func, sessions), 0 )
+
+  @classmethod
+  def amount_of_viewed_moderately_relevant_documents(cls, filter_func = Filterable.identity_filter):
+    sessions = cls.get_store().values()
+    return reduce( lambda acc, session: acc + len(session.viewed_moderately_relevant_documents()), filter(filter_func, sessions), 0 )
+
+  @classmethod
+  def amount_of_seen_non_relevant_documents(cls, filter_func = Filterable.identity_filter):
+    sessions = cls.get_store().values()
+    return reduce( lambda acc, session: acc + len(session.seen_non_relevant_documents()), filter(filter_func, sessions), 0 )
+
+  @classmethod
+  def amount_of_viewed_non_relevant_documents(cls, filter_func = Filterable.identity_filter):
+    sessions = cls.get_store().values()
+    return reduce( lambda acc, session: acc + len(session.viewed_non_relevant_documents()), filter(filter_func, sessions), 0 )
--- session.py
+++ session.py
@@ -4,9 +4,10 @@ import itertools
 from data_record import DataRecord
 from has_actions import HasActions
 from filterable import Filterable
+from has_documents import HasDocuments
 
 
-class Session(DataRecord, HasActions, Filterable):
+class Session(DataRecord, HasActions, Filterable, HasDocuments):
 
   def __init__(self, session_id, user, topic, condition):
     DataRecord.__init__( self, session_id )
@@ -14,53 +15,11 @@ class Session(DataRecord, HasActions, Filterable):
     self.user = user
     self.condition = condition
 
-    self.seen_documents = {}
-    self.viewed_documents = {}
-    self.marked_relevant_documents = {}
     self.queries = {}
 
   def add_query(self, query):
     self.queries[query.record_id] = query
     query.session = self
-
-  def add_seen_documents(self, *documents):
-    for document in documents:
-      self.seen_documents[ document.record_id ] = document
-
-  def add_viewed_documents( self, *documents ):
-    for document in documents:
-      self.viewed_documents[ document.record_id ] = document
-
-  def add_marked_relevant_documents( self, *documents ):
-    for document in documents:
-      self.marked_relevant_documents[ document.record_id ] = document
-
-  def seen_highly_relevant_documents(self):
-    return [document for document in self.seen_documents.values() if document.is_highly_relevant_for_topic( self.topic )]
-
-  def seen_moderately_relevant_documents(self):
-    return [document for document in self.seen_documents.values() if document.is_moderately_relevant_for_topic( self.topic )]
-
-  def seen_non_relevant_documents(self):
-    return [document for document in self.seen_documents.values() if not document.is_relevant_for_topic( self.topic )]
-
-  def viewed_highly_relevant_documents(self):
-    return [document for document in self.viewed_documents.values() if document.is_highly_relevant_for_topic( self.topic )]
-
-  def viewed_moderately_relevant_documents(self):
-    return [document for document in self.viewed_documents.values() if document.is_moderately_relevant_for_topic( self.topic )]
-
-  def viewed_non_relevant_documents(self):
-    return [document for document in self.viewed_documents.values() if not document.is_relevant_for_topic( self.topic )]
-
-  def marked_highly_relevant_documents(self):
-    return [document for document in self.marked_relevant_documents.values() if document.is_highly_relevant_for_topic( self.topic )]
-
-  def marked_moderately_relevant_documents(self):
-    return [document for document in self.marked_relevant_documents.values() if document.is_moderately_relevant_for_topic( self.topic )]
-
-  def marked_non_relevant_documents(self):
-    return [document for document in self.marked_relevant_documents.values() if not document.is_relevant_for_topic( self.topic )]
 
   def duration_in_seconds(self):
     first_timestamp = self.actions[0].timestamp
@@ -104,51 +63,6 @@ class Session(DataRecord, HasActions, Filterable):
   @classmethod
   def build_session_id( cls, user_id, topic_id ):
     return str( user_id ) + '-' + str( topic_id )
-
-  @classmethod
-  def amount_of_seen_highly_relevant_documents(cls, filter_func = Filterable.identity_filter):
-    sessions = cls.get_store().values()
-    return reduce( lambda acc, session: acc + len(session.seen_highly_relevant_documents()), filter(filter_func, sessions), 0 )
-
-  @classmethod
-  def amount_of_viewed_highly_relevant_documents(cls, filter_func = Filterable.identity_filter):
-    sessions = cls.get_store().values()
-    return reduce( lambda acc, session: acc + len(session.viewed_highly_relevant_documents()), filter(filter_func, sessions), 0 )
-
-  @classmethod
-  def amount_of_marked_highly_relevant_documents(cls, filter_func = Filterable.identity_filter):
-    sessions = cls.get_store().values()
-    return reduce( lambda acc, session: acc + len(session.marked_highly_relevant_documents()), filter(filter_func, sessions), 0 )
-
-  @classmethod
-  def amount_of_marked_moderately_relevant_documents(cls, filter_func = Filterable.identity_filter):
-    sessions = cls.get_store().values()
-    return reduce( lambda acc, session: acc + len(session.marked_moderately_relevant_documents()), filter(filter_func, sessions), 0 )
-
-  @classmethod
-  def amount_of_marked_non_relevant_documents(cls, filter_func = Filterable.identity_filter):
-    sessions = cls.get_store().values()
-    return reduce( lambda acc, session: acc + len(session.marked_non_relevant_documents()), filter(filter_func, sessions), 0 )
-
-  @classmethod
-  def amount_of_seen_moderately_relevant_documents(cls, filter_func = Filterable.identity_filter):
-    sessions = cls.get_store().values()
-    return reduce( lambda acc, session: acc + len(session.seen_moderately_relevant_documents()), filter(filter_func, sessions), 0 )
-
-  @classmethod
-  def amount_of_viewed_moderately_relevant_documents(cls, filter_func = Filterable.identity_filter):
-    sessions = cls.get_store().values()
-    return reduce( lambda acc, session: acc + len(session.viewed_moderately_relevant_documents()), filter(filter_func, sessions), 0 )
-
-  @classmethod
-  def amount_of_seen_non_relevant_documents(cls, filter_func = Filterable.identity_filter):
-    sessions = cls.get_store().values()
-    return reduce( lambda acc, session: acc + len(session.seen_non_relevant_documents()), filter(filter_func, sessions), 0 )
-
-  @classmethod
-  def amount_of_viewed_non_relevant_documents(cls, filter_func = Filterable.identity_filter):
-    sessions = cls.get_store().values()
-    return reduce( lambda acc, session: acc + len(session.viewed_non_relevant_documents()), filter(filter_func, sessions), 0 )
 
   @classmethod
   def average_duration_in_seconds(cls, filter_func = Filterable.identity_filter):
