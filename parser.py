9b482f406ebb56ebd55ad8d3ed2a58cda0819eb7
Teemu Pääkkönen <teemu@flockler.com>
2017-08-21 21:09:59 +0300

    add parser utility

--- parser.py
+++ parser.py
@@ -1,11 +1,19 @@
 import sys
-import optparse
 import os
+import csv
+
+
+import numpy
+
+
+import data_export
+import statistics
 
 
 from result_file import ResultFile
 from queries_summary_file import QueriesSummaryFile
 from log_file import LogFile
+from combined_log_file import CombinedLogFile
 from document import Document
 from query import Query
 from topic import Topic
@@ -13,21 +21,16 @@ from user import User
 from condition import Condition
 from action import Action
 from session import Session
+from relevance import Relevance
 from filterable import Filterable
+from session_time_slice import SessionTimeSlice
+from cli import CLI
 
 
 def error_exit():
-  sys.stderr.write( 'ERROR: missing argument(s).\n' )
+  sys.stderr.write('ERROR: missing argument(s).\n')
   CLI.parser.print_help()
   sys.exit(1)
-
-
-class CLI: pass
-CLI.parser = optparse.OptionParser()
-CLI.parser.add_option( "-q", "--queries", dest = "queries", help = "Queries csv file", metavar="FILE")
-CLI.parser.add_option( "-l", "--logs", dest = "logs", help = "Path to log files containing directory", metavar="DIR")
-CLI.parser.add_option( "-r", "--results", dest = "results", help = "Path to result files containing directory", metavar="DIR")
-CLI.parsedArgs = CLI.parser.parse_args()
 
 
 def get_cli_options():
@@ -35,10 +38,9 @@ def get_cli_options():
   return options
 
 
-def parse_result_files():
+def parse_result_files(cli_options):
   result_files = []
-  opts = get_cli_options()
-  results_dir = opts.results
+  results_dir = cli_options.results
   if results_dir is None: error_exit()
   for dirname, dirnames, filenames in os.walk( results_dir ):
     files_count = len( filenames )
@@ -54,158 +56,50 @@ def parse_result_files():
   return result_files
 
 
-def parse_log_files():
+def parse_log_files(cli_options):
   log_files = []
-  opts = get_cli_options()
-  log_dir = opts.logs
+  log_dir = cli_options.logs
   if log_dir is None: error_exit()
   for dirname, dirnames, filenames in os.walk( log_dir ):
     files_count = len( filenames )
-    every_tenth = int( files_count / 10 )
+    every_tenth = int( files_count / 10 ) or 1
     sys.stdout.write( "Parsing %i log files" % files_count )
     for idx, filename in enumerate(filenames):
       if not filename.endswith('.log'): continue
       sys.stdout.write( '.' )
       if idx % every_tenth == 0: sys.stdout.write(str(idx))
       sys.stdout.flush()
-      log_files.append( LogFile( os.path.join( dirname, filename ) ) )
+      if cli_options.use_combined_log_parser:
+        log_files.append( CombinedLogFile( os.path.join( dirname, filename ) ) )
+      else:
+        log_files.append( LogFile( os.path.join( dirname, filename ) ) )
   sys.stdout.write( '\n' )
   return log_files
 
 
-def get_queries_file_name():
-  opts = get_cli_options()
-  if opts.queries is None: error_exit()
-  return opts.queries
+def parse_gains(cli_options):
+  gains = cli_options.gains
+  if gains is None:
+    error_exit()
+  return [int(i) for i in gains.split(",")]
 
 
-result_files = parse_result_files()
-log_files = parse_log_files()
-queries_file = QueriesSummaryFile( get_queries_file_name() )
-
-print "Queries: %i" % len(Query.store)
-print "Topics: %i: %s" % (len(Topic.store), Topic.store.keys())
-print "Users: %i" % len(User.store)
-print "Sessions: %i" % len(Session.store)
-print "Documents: %i" % len(Document.store)
-print "Conditions: %i: %s" % (len(Condition.store), Condition.store.keys())
-print "Action types: %i: %s" % (len(Action.type_dict.keys()), Action.type_dict.keys())
+def get_queries_file_name(cli_options):
+  if cli_options.queries is None:
+    error_exit()
+  return cli_options.queries
 
 
-def print_stats_for_filter(fil):
-  print "Highly relevant docs seen across sessions: %i, of which viewed: %i, of which marked relevant: %i" % (
-    Session.amount_of_seen_highly_relevant_documents( fil ),
-    Session.amount_of_viewed_highly_relevant_documents( fil ),
-    Session.amount_of_marked_highly_relevant_documents( fil ))
-  print "Moderately relevant docs seen across sessions: %i, of which viewed: %i, of which marked relevant: %i" % (
-    Session.amount_of_seen_moderately_relevant_documents(fil),
-    Session.amount_of_viewed_moderately_relevant_documents(fil),
-    Session.amount_of_marked_moderately_relevant_documents(fil))
-  print "Non-relevant docs seen across sessions: %i, of which viewed: %i, of which marked relevant: %i" % (
-    Session.amount_of_seen_non_relevant_documents(fil),
-    Session.amount_of_viewed_non_relevant_documents(fil),
-    Session.amount_of_marked_non_relevant_documents(fil))
-  print "Highly relevant docs seen across queries: %i, of which viewed: %i, of which marked relevant: %i" % (
-    Query.amount_of_seen_highly_relevant_documents( fil ),
-    Query.amount_of_viewed_highly_relevant_documents( fil ),
-    Query.amount_of_marked_highly_relevant_documents( fil ))
-  print "Moderately relevant docs seen across queries: %i, of which viewed: %i, of which marked relevant: %i" % (
-    Query.amount_of_seen_moderately_relevant_documents(fil),
-    Query.amount_of_viewed_moderately_relevant_documents(fil),
-    Query.amount_of_marked_moderately_relevant_documents(fil))
-  print "Non-relevant docs seen across queries: %i, of which viewed: %i, of which marked relevant: %i" % (
-    Query.amount_of_seen_non_relevant_documents(fil),
-    Query.amount_of_viewed_non_relevant_documents(fil),
-    Query.amount_of_marked_non_relevant_documents(fil))
-  print "Average document reading time: %s sec" % Session.global_average_document_reading_time_in_seconds( fil )
-  print "Average query formulation time: %s sec" % Query.average_formulation_time_in_seconds( fil )
-  print "Average snippet scanning time: %s sec" % Session.global_average_snippet_scanning_time_in_seconds( fil )
-  print "Average query last rank reached: %s" % Query.average_last_rank_reached( fil )
-
-
-print "--- ALL SESSIONS (NO PRACTICE) ---"
-print_stats_for_filter( Filterable.practice_topic_reject_filter )
-
-print "--- NON-DELAYED SESSIONS (NO PRACTICE) ---"
-print_stats_for_filter( Filterable.combine_filters( Filterable.no_delays_filter, Filterable.practice_topic_reject_filter ) )
-
-print "--- NON-DELAYED SESSIONS (TOPIC 435) ---"
-print_stats_for_filter( Filterable.combine_filters( Filterable.no_delays_filter, Filterable.practice_topic_reject_filter, Filterable.topic_filter(435) ) )
-
-print "--- NON-DELAYED SESSIONS (TOPIC 347) ---"
-print_stats_for_filter( Filterable.combine_filters( Filterable.no_delays_filter, Filterable.practice_topic_reject_filter, Filterable.topic_filter(347) ) )
-
-print "--- QUERY DELAY SESSIONS (NO PRACTICE) ---"
-print_stats_for_filter( Filterable.combine_filters( Filterable.query_delay_filter, Filterable.practice_topic_reject_filter ) )
-
-print "--- DOCUMENT DELAY SESSIONS (NO PRACTICE) ---"
-print_stats_for_filter( Filterable.combine_filters( Filterable.document_delay_filter, Filterable.practice_topic_reject_filter ) )
-
-print "--- COMBINED DELAY SESSIONS (NO PRACTICE) ---"
-print_stats_for_filter( Filterable.combine_filters( Filterable.combined_delay_filter, Filterable.practice_topic_reject_filter ) )
-
-print "-- MISC --"
-print "Average session duration: %s sec" % Session.average_duration_in_seconds()
-
-# print "Mark-doc-as-relevant actions: %i" % (
-#   len(Action.by_type('DOC_MARKED_RELEVANT')))
-# print "Mark-doc-as-relevant actions with a moderately relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.moderately_relevant_filter)))
-# print "Mark-doc-as-relevant actions with a highly relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.highly_relevant_filter)))
-
-# print "Mark-doc-as-viewed actions with no delays: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_VIEWED', Action.no_delays_filter)))
-# print "Mark-doc-as-viewed actions with no delays and moderately relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_VIEWED', Action.combine_filters( Action.no_delays_filter, Action.moderately_relevant_filter))))
-# print "Mark-doc-as-viewed actions with no delays and highly relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_VIEWED', Action.combine_filters( Action.no_delays_filter, Action.highly_relevant_filter))))
-
-# print "Mark-doc-as-relevant actions with no delays: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.no_delays_filter)))
-# print "Mark-doc-as-relevant actions with no delays and moderately relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.combine_filters( Action.no_delays_filter, Action.moderately_relevant_filter))))
-# print "Mark-doc-as-relevant actions with no delays and highly relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.combine_filters( Action.no_delays_filter, Action.highly_relevant_filter))))
-
-# print "Mark-doc-as-viewed actions with query delays: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_VIEWED', Action.query_delay_filter)))
-# print "Mark-doc-as-viewed actions with query delays and moderately relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_VIEWED', Action.combine_filters( Action.query_delay_filter, Action.moderately_relevant_filter))))
-# print "Mark-doc-as-viewed actions with query delays and highly relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_VIEWED', Action.combine_filters( Action.query_delay_filter, Action.highly_relevant_filter))))
-
-# print "Mark-doc-as-relevant actions with query delays: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.query_delay_filter)))
-# print "Mark-doc-as-relevant actions with query delays and moderately relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.combine_filters( Action.query_delay_filter, Action.moderately_relevant_filter))))
-# print "Mark-doc-as-relevant actions with query delays and highly relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.combine_filters( Action.query_delay_filter, Action.highly_relevant_filter))))
-
-# print "Mark-doc-as-viewed actions with document delays: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_VIEWED', Action.document_delay_filter)))
-# print "Mark-doc-as-viewed actions with document delays and moderately relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_VIEWED', Action.combine_filters( Action.document_delay_filter, Action.moderately_relevant_filter))))
-# print "Mark-doc-as-viewed actions with document delays and highly relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_VIEWED', Action.combine_filters( Action.document_delay_filter, Action.highly_relevant_filter))))
-
-# print "Mark-doc-as-relevant actions with document delays: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.document_delay_filter)))
-# print "Mark-doc-as-relevant actions with document delays and moderately relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.combine_filters( Action.document_delay_filter, Action.moderately_relevant_filter))))
-# print "Mark-doc-as-relevant actions with document delays and highly relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.combine_filters( Action.document_delay_filter, Action.highly_relevant_filter))))
-
-# print "Mark-doc-as-viewed actions with both delays: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_VIEWED', Action.combined_delay_filter)))
-# print "Mark-doc-as-viewed actions with both delays and moderately relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_VIEWED', Action.combine_filters( Action.combined_delay_filter, Action.moderately_relevant_filter))))
-# print "Mark-doc-as-viewed actions with both delays and highly relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_VIEWED', Action.combine_filters( Action.combined_delay_filter, Action.highly_relevant_filter))))
-
-# print "Mark-doc-as-relevant actions with both delays: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.combined_delay_filter)))
-# print "Mark-doc-as-relevant actions with both delays and moderately relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.combine_filters( Action.combined_delay_filter, Action.moderately_relevant_filter))))
-# print "Mark-doc-as-relevant actions with both delays and highly relevant doc: %i" % (
-#   len(Action.filter_by_type('DOC_MARKED_RELEVANT', Action.combine_filters( Action.combined_delay_filter, Action.highly_relevant_filter))))
+def parse():
+  cli_options = get_cli_options()
+  gains = parse_gains(cli_options)
+  result_files = parse_result_files(cli_options)
+  log_files = parse_log_files(cli_options)
+  queries_file = QueriesSummaryFile(get_queries_file_name(cli_options))
+  return {
+      'cli_options': cli_options,
+      'gains': gains,
+      'result_files': result_files,
+      'log_files': log_files,
+      'queries_file': queries_file
+  }
